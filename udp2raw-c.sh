c3b4d24159309a987d9e640cc13a22f1408ebb28
lmc999 <39726914+lmc999@users.noreply.github.com>
2019-01-16 09:47:08 +0800

    Update udp2raw-c.sh

--- udp2raw-c.sh
+++ udp2raw-c.sh
@@ -2,13 +2,13 @@
 cd /usr/local/bin
 curl -o udp2raw https://raw.githubusercontent.com/lmc999/OpenvpnForGames/master/udp2raw
 chmod +x /usr/local/bin/udp2raw
-nohup udp2raw.exe -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd >udp2raw.log 2>&1 &
+nohup udp2raw -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd >udp2raw.log 2>&1 &
 
 cat > /etc/rc.d/init.d/udp<<-EOF
 #!/bin/sh
 #chkconfig: 2345 80 90
 #description:udp
-nohup udp2raw.exe -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd >udp2raw.log 2>&1 &
+nohup udp2raw -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd >udp2raw.log 2>&1 &
 EOF
 
 chmod +x /etc/rc.d/init.d/udp
