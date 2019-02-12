7ce024168dbd991ad6c92615d7c319490306fdb0
lmc999 <39726914+lmc999@users.noreply.github.com>
2019-02-12 12:33:27 +0800

    Create udp-for-game-S-unbuntu.sh

---
+++ udp-for-game-S-unbuntu.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#适用ubuntu 16.04
+
+#安装必要组件
+apt-get install wget -y
+apt-get install curl -y
+
+#清除规则
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+service iptables save
+
+#启用转发
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+#永久转发
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+
+#下载udpspeeder和udp2raw （amd64版）
+cd /usr/local/bin
+curl -o speederv2 https://raw.githubusercontent.com/lmc999/OpenvpnForGames/master/speederv2
+curl -o udp2raw https://raw.githubusercontent.com/lmc999/OpenvpnForGames/master/udp2raw
+chmod +x speederv2 udp2raw
+
+#启动udpspeeder和udp2raw
+nohup speederv2 -s -l0.0.0.0:9999 -r127.0.0.1:11193 -f2:2 --mode 0 --timeout 0 >speeder.log 2>&1 &
+nohup udp2raw -s -l0.0.0.0:9898 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 &
+nohup speederv2 -s -l0.0.0.0:8999 -r127.0.0.1:11931 -f2:2 --mode 0 --timeout 0 >speeder.log 2>&1 &
+nohup udp2raw -s -l0.0.0.0:8898 -r 127.0.0.1:8999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 &
+
+#增加自启动脚本
+echo "nohup speederv2 -s -l0.0.0.0:9999 -r127.0.0.1:11193 -f2:4 --mode 0 --timeout 0 >speeder.log 2>&1 &" | sudo tee -a /etc/rc.local
+echo "nohup udp2raw -s -l0.0.0.0:9898 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 &" | sudo tee -a /etc/rc.local
+echo "nohup speederv2 -s -l0.0.0.0:8999 -r127.0.0.1:11931 -f2:2 --mode 0 --timeout 0 >speeder.log 2>&1 &" | sudo tee -a /etc/rc.local
+echo "nohup udp2raw -s -l0.0.0.0:8898 -r 127.0.0.1:8999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 &" | sudo tee -a /etc/rc.local
