85f72d5f561c2107e23f1075d531ae6d135ca97d
lmc999 <39726914+lmc999@users.noreply.github.com>
2019-02-16 11:11:59 +0800

    Update udp-for-game-C-ubuntu.sh

--- udp-for-game-C-ubuntu.sh
+++ udp-for-game-C-ubuntu.sh
@@ -29,7 +29,7 @@ nohup speederv2 -c -l0.0.0.0:2098 -r127.0.0.1:8999 -f2:4 --mode 0 --report 10 --
 nohup udp2raw -c -r44.55.66.77:8898 -l 127.0.0.1:8999 --raw-mode faketcp -k passwd >udp2raw.log 2>&1 &
 
 #增加自启动脚本
-echo "nohup speederv2 -c -l0.0.0.0:2099 -r127.0.0.1:9999 -f2:4 --mode 0 --report 10 --timeout 0 >speeder.log 2>&1 &" | sudo tee -a /etc/rc.local
+echo "nohup speederv2 -c -l0.0.0.0:2099 -r127.0.0.1:9999 -f2:4 --mode 0 --report 10 --timeout 0 >speeder1.log 2>&1 &" | sudo tee -a /etc/rc.local
 echo "nohup udp2raw -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd >udp2raw.log 2>&1 &" | sudo tee -a /etc/rc.local
-echo "nohup speederv2 -c -l0.0.0.0:2098 -r127.0.0.1:8999 -f2:4 --mode 0 --report 10 --timeout 0 >speeder.log 2>&1 &" | sudo tee -a /etc/rc.local
+echo "nohup speederv2 -c -l0.0.0.0:2098 -r127.0.0.1:8999 -f2:4 --mode 0 --report 10 --timeout 0 >speeder2.log 2>&1 &" | sudo tee -a /etc/rc.local
 echo "nohup udp2raw -c -r44.55.66.77:8898 -l 127.0.0.1:8999 --raw-mode faketcp -k passwd >udp2raw.log 2>&1 &" | sudo tee -a /etc/rc.local
