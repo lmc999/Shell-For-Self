313394587ae6ca45e62eb8c97cc7064a14688ed6
lmc999 <39726914+lmc999@users.noreply.github.com>
2020-07-14 21:35:39 +0800

    Create asw_region_ip.py
    get AWS specific region EC2 IP prefix

---
+++ asw_region_ip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import requests
+
+ip_ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()['prefixes']
+amazon_ips = [item['ip_prefix'] for item in ip_ranges if item["region"] == "ap-northeast-2"] #ap-northeast-2为韩国首尔
+ec2_ips = [item['ip_prefix'] for item in ip_ranges if item["service"] == "EC2"]
+
+amazon_ips_less_ec2=[]
+     
+for ip in amazon_ips:
+    if ip not in ec2_ips:
+        amazon_ips_less_ec2.append(ip)
+
+for ip in amazon_ips_less_ec2: print(str(ip))
