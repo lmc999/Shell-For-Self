a411348cf08ef1ba4b36821f808de53c7f213445
lmc999 <39726914+lmc999@users.noreply.github.com>
2020-07-14 22:20:24 +0800

    Update asw_region_ip.py

--- asw_region_ip.py
+++ asw_region_ip.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-#-*-coding:utf-8-*-
 import requests
 
 ip_ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()['prefixes']
-amazon_ips = [item['ip_prefix'] for item in ip_ranges if item["region"] == "ap-northeast-2"] #ap-northeast-2为韩国首尔
+amazon_ips = [item['ip_prefix'] for item in ip_ranges if item["region"] == "ap-northeast-2"] #ap-northeast-2 is seoul
 ec2_ips = [item['ip_prefix'] for item in ip_ranges if item["service"] == "EC2"]
 
 amazon_ips_less_ec2=[]
