5f966ff0649dd9c93638036edbb1dbd5d23f6b10
lmc999 <39726914+lmc999@users.noreply.github.com>
2020-07-27 23:18:07 +0800

    Create jp_up.sh

---
+++ jp_up.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+nohup ping -I tun0 10.211.254.254 >/dev/null 2>&1 &
+ip route add default via $(ip addr show tun0 | grep inet | awk '{print $4}' | cut -f1 -d"/" | awk NR==1)  dev tun0 table media >/dev/null 2>&1
