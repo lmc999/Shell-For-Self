2a694d86f71b53dcde37feec34c282cad0edb9ac
lmc999 <39726914+lmc999@users.noreply.github.com>
2020-08-26 22:55:05 +0800

    Update tw_up.sh

--- tw_up.sh
+++ tw_up.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-nohup ping -I tun2 10.211.254.254 >/dev/null 2>&1 &
-ip route add default via $(ip addr show tun2 | grep inet | awk '{print $4}' | cut -f1 -d"/" | awk NR==1)  dev tun2 table taiwan >/dev/null 2>&1
+nohup ping 10.211.254.254 & >/dev/null 2>&1 
+ip route add default via $(ip addr show tun1 | grep inet | awk '{print $4}' | cut -f1 -d"/" | awk NR==1)  dev tun1 table media >/dev/null 2>&1
