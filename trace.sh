6bf3c3b5be941772fbdce0c81d642e057d4b665d
lmc999 <39726914+lmc999@users.noreply.github.com>
2021-06-13 15:35:45 +0800

    Update trace.sh

--- trace.sh
+++ trace.sh
@@ -4,6 +4,8 @@ yum install unzip -y
 yum install mtr -y
 wget -O ./besttrace https://raw.githubusercontent.com/lmc999/Shell-For-Self/master/besttrace
 chmod +x besttrace
+wget -O ./worsttrace https://pkg.wtrace.app/linux/worsttrace
+chmod +x worsttrace
 wget https://github.com/zu1k/nali/releases/download/v0.2.2/nali-linux-amd64-v0.2.2.gz
 gunzip nali-linux-amd64-v0.2.2.gz
 mv ./nali-linux-amd64-v0.2.2 /usr/bin/nali
