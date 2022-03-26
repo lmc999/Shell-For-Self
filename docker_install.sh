52e42d3cadb405930723c88cb0845623d6439ce6
lmc999 <39726914+lmc999@users.noreply.github.com>
2022-03-26 13:51:01 +0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -15,13 +15,13 @@ checkOS() {
     os_version=$(cat /etc/os-release)
     isDebian=$(echo $os_version | grep ID=debian)
     isUbuntu=$(echo $os_version | grep ID=ubuntu)
-    if [ -n "$isUbuntu"]; then
+    if [ -n "$isUbuntu" ]; then
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo \
         "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-    elif [ -n "$isDebian"]; then
+    elif [ -n "$isDebian" ]; then
         curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
