2da38065be93e65afb49ae50210e934dec8ca329
lmc999 <39726914+lmc999@users.noreply.github.com>
2023-01-19 22:10:08 +0800

    Update trace.sh

--- trace.sh
+++ trace.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
+CPUArch=$(uname -m)
+if [[ "$CPUArch" == "aarch64" ]]; then
+  isARM=arm
+fi  
 apt install unzip -y
 wget -O /tmp/besttrace.zip https://cdn.ipip.net/17mon/besttrace4linux.zip
 unzip -d /tmp /tmp/besttrace.zip
-mv /tmp/besttrace ~/besttrace
+mv /tmp/besttrace${isARM} ~/besttrace
 chmod +x ~/besttrace
 rm -rf /tmp/besttrace*
-wget -O ./worsttrace https://pkg.wtrace.app/linux/worsttrace
-chmod +x worsttrace
-wget https://github.com/zu1k/nali/releases/download/v0.2.2/nali-linux-amd64-v0.2.2.gz
-gunzip nali-linux-amd64-v0.2.2.gz
-mv ./nali-linux-amd64-v0.2.2 /usr/bin/nali
-chmod +x /usr/bin/nali
